98edc0e9a7fda3c482a841a04d00d082a7cf9f2d
nazim <djeblounnazim2@gmail.com>
2025-03-17 00:45:08 +0100

    added purge CSS to remove unused css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 // import { heroui } from "@heroui/react";
 
 export default {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
