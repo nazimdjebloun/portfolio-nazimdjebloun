2914972972bed8a525c65593e5ebb75f0a77c3f1
nazim <djeblounnazim2@gmail.com>
2025-03-17 00:02:30 +0100

    added vercel speed insights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export default function Document() {
   return (
-    <Html lang="en" className={` bg-background text-black`}>
+    <Html lang="en" className={` bg-background text-foreground`}>
       <Head>
         {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js"></script> */}
         <meta
@@ -13,6 +13,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
   );
